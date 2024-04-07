6b58d0b629de3bbad61bea5402ffd20f78bac01c
jonatan-lagland <jonatan.lagland@gmail.com>
2024-04-07 12:59:44 +0300

    Implement logging for userContextMenuCommands

---
+++ events/contextMenuListener.js
@@ -0,0 +1,26 @@
+const { Events, ChannelType } = require('discord.js');
+
+module.exports = {
+    name: Events.InteractionCreate,
+    async execute(interaction) {
+        // Handling chat input commands
+        if (!interaction.isUserContextMenuCommand()) {
+            return;
+        }
+
+        // Fetch the #mod-bot channel
+        const modBotChannel = interaction.guild.channels.cache.find(channel => channel.name === 'mod-bot' && channel.type === ChannelType.GuildText);
+        const targetUser = await interaction.guild.members.fetch(interaction.targetId).catch(console.error);
+
+        if (!modBotChannel) {
+            console.log("Could not find the #mod-bot channel.");
+            return;
+        }
+
+        // Compose a message about the interaction
+        const interactionInfo = `**${interaction.user.globalName}** (${interaction.user.tag}) used the command '${interaction.commandName}' on ${targetUser}.`;
+
+        // Send the message to the #mod-bot channel
+        await modBotChannel.send({ content: interactionInfo }).catch(console.error);
+    },
+};
