a1890ba02c5e9797e1ce9a7d979cf42deeb379bb
jonatan-lagland <jonatan.lagland@gmail.com>
2024-04-11 21:59:56 +0300

    Refractor cat command to answer questions

--- commands/utility/cat.js
+++ commands/utility/cat.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 require('dotenv').config();
 const tenor_token = process.env.TENOR_TOKEN;
@@ -7,10 +7,21 @@ const client_key = process.env.CLIENT_ID;
 let lastCommandTime = 0;
 const cooldownDuration = 3000;
 
+function ensureQuestionMark(str) {
+    if (!str.endsWith('?')) {
+        str += '?';
+    }
+    return str;
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('cat')
-        .setDescription('Get a gif of a cat!'),
+        .setName('mrporcebot')
+        .setDescription('Let Mr. PorceBot answer your fate.')
+        .addStringOption(option =>
+            option.setName('question')
+                .setRequired(true)
+                .setDescription('State your question.')),
 
     async execute(interaction) {
         try {
@@ -21,15 +32,22 @@ module.exports = {
             }
             lastCommandTime = currentTime;
 
+
+            const input = interaction.options.getString('question');
             const cat = "cat";
-            const search_limit = 30;
-            const search_fluff = ["tired", "silly", "exhausted", "kiss", "love", "fast", "cute", "paws", "hug", "sus", "stare", "smile", "slap", "yapping", "ponder", "chill", "lick", "dance", "yippee", "shock", "cozy", "snack"]
+            const search_limit = 100;
+            const user = interaction.user;
+            //const search_fluff = ["tired", "silly", "exhausted", "kiss", "love", "fast", "cute", "paws", "hug", "sus", "stare", "smile", "slap", "yapping", "ponder", "chill", "lick", "dance", "yippee", "shock", "cozy", "snack"]
+            const search_fluff = ["no", "yes", "peek", "angry", "stare", "love", "slap", "yippee"]
+            //const answer_fluff = ["This is what PorceBot has to say about it...", "What does PorceBot think? Well...", "PorceBot answered with...", "Well, you see...", ""]
             const index = Math.floor(Math.random() * search_fluff.length);
             const search_term = cat + " " + search_fluff[index];
             const response = await fetch("https://tenor.googleapis.com/v2/search?q=" + search_term + "&key=" + tenor_token + "&client_key=" + client_key + "&limit=" + search_limit);
             const data = await response.json();
             const randomIndex = Math.floor(Math.random() * data.results.length);
-            await interaction.reply({ content: data.results[randomIndex].url }).catch(console.error);
+            const inputWithQuestionMark = ensureQuestionMark(input)
+
+            await interaction.reply({ content: `${user} has asked a question...\n` + `# "${inputWithQuestionMark}"\n` + '### *This is what PorceBot has to say about it:*\n' + data.results[randomIndex].url }).catch(console.error);
         } catch (error) {
             console.error('Error fetching cat gif:', error);
         }
